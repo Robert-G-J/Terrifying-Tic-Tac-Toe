a4c80db8f5ae7434fc4060a01e5fdb0cc1f23756
Robert Jones <robert.g.jones@gmail.com>
2017-10-11 00:41:11 +0100

    Refactor feature tests to use 'let' syntax
    Changing to this syntax drys the code and lazy loads the variables
    before each of the tests, improving testing speed.

--- spec/ui_spec.rb
+++ spec/ui_spec.rb
@@ -2,17 +2,21 @@ require 'game'
 require 'board'
 
 describe 'Features of UI: ' do
-
-  it 'Game raises an error if player takes a field that is already taken' do
-    human_player = Human.new
-    computer_player = Computer.new
-    board = Board.new
-    game = Game.new(
+    let(:human_player) { Human.new }
+    let(:computer_player) { Computer.new }
+    let(:board) { Board.new }
+    let(:game) { Game.new(
       player1: human_player,
       player2: computer_player,
       board: board
-    )
+    ) }
+
+  it 'Game raises an error if player takes a field that is already taken' do
     game.place_token('X', 0)
     expect { game.place_token('O', 0) }.to raise_exception('Cell Occupied')
   end
+
+  xit 'Game is over if all fields are taken' do
+  end
+
 end
