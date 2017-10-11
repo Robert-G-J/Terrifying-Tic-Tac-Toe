4ccf4bc3cd720244dd483a3922156fb650e31be3
Robert Jones <robert.g.jones@gmail.com>
2017-10-11 14:24:10 +0100

    Let the Board know if a game is won by Row
    This is a partial solution and first step; and minimal step towards
    solving for diagonals and columns.
    
    The Board::game_over? method slices it's board array into three
    different arrays and then checks if any have three 'X' tokens in a row.
    
    The tests are brittle because they only test for 'X'; they need to be
    changed to allow any token that a player might have

--- lib/board.rb
+++ lib/board.rb
@@ -10,4 +10,19 @@ class Board
   def occupied?(index)
     state[index.to_i] != nil
   end
+
+  def game_over?
+    winning_row?
+  end
+
+  def winning_row?
+    value = rows.any? do |row|
+      row.count('X') == 3
+    end
+    value
+  end
+
+  def rows
+    state.each_slice(3).to_a
+  end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -4,7 +4,6 @@ describe Board do
   subject(:board) { described_class.new }
 
   describe '#occupied' do
-
     before do
       board.state = ['X']
     end
@@ -14,12 +13,29 @@ describe Board do
         expect(board.occupied?(0)).to be true
       end
     end
-    
+
     context 'false' do
       it 'has no token' do
         board.state = []
-        p board.occupied?(0)
         expect(board.occupied?(0)).to be false
+      end
+    end
+  end
+
+  describe '#game_over?' do
+    context 'true' do
+      xit 'is a tie' do
+        board.state = ['X', 'O', 'X',
+                       'O', 'X', 'O',
+                       'X', 'O', 'X']
+        expect(board.game_over?).to be true
+      end
+
+      it 'is won by row' do
+        board.state = ['X', 'X', 'X', 
+                       'O', nil, nil,
+                       nil, 'O', nil]
+        expect(board.game_over?).to be true
       end
     end
   end
