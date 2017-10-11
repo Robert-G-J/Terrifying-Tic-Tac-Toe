79e110c9dbb9ad716ef246c084ccf2b99382929c
Robert Jones <robert.g.jones@gmail.com>
2017-10-11 18:11:06 +0100

    Create a run file that runs the game
    The top level main.rb file will be used to run the game. It is currently
    committed with the equivalent of 'Hello World' code, where it welcomes a
    person to the game.

---
+++ main.rb
@@ -0,0 +1,16 @@
+require './lib/ui'
+require './lib/game'
+require './lib/human'
+require './lib/computer'
+require './lib/board'
+
+def run_game
+  ui = UI.new
+  board = Board.new
+  human = Human.new(token:'🏄')
+  computer = Computer.new(token:'🤖') 
+  game = Game.new(board: board, player1: human, player2: computer)
+  ui.welcome
+end
+
+run_game
