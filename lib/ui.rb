e6f5faeabae77111f8091d88f169111c9edea284
Robert Jones <robert.g.jones@gmail.com>
2017-10-11 18:10:03 +0100

    UI can request integer value for move choice
    Message is output to stdout; will be used in conjunction with 'gets' to
    trap the user information

--- lib/ui.rb
+++ lib/ui.rb
@@ -2,4 +2,8 @@ class UI
   def welcome
     puts 'Welcome to Tic-Tac-Toe'
   end
+
+ def pick_square
+   puts "Pick a square from 1-9"
+ end
 end
--- spec/ui_spec.rb
+++ spec/ui_spec.rb
@@ -7,4 +7,9 @@ describe UI do
     welcome_msg = "Welcome to Tic-Tac-Toe\n"
     expect { ui.welcome }.to output(welcome_msg).to_stdout
   end
+
+  it 'Requests the player pick a position' do
+    message = "Pick a square from 1-9\n"
+    expect { ui.pick_square }.to output(message).to_stdout
+  end
 end
